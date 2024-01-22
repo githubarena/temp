054a19d3afdb7f7a15a35672cd3ca29a20bd6b32
githubarena <156434742+githubarena@users.noreply.github.com>
2024-01-22 10:04:20 +0530

    Create Kalu.cpp

---
+++ 900/Kalu.cpp
@@ -0,0 +1,28 @@
+GALTU#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << (count(s.begin(), s.end(), 'A') > n / 2 ? "Anton" : count(s.begin(), s.end(), 'D') > n / 2 ? "Danik" : "Friendship");#include <bits/std#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << (count(s.begin(), s.end(), 'A') > n / 2 ? "Anton" : count(s.begin(), s.end(), 'D') > n / 2 ? "Danik" : "Friendship");
+}c++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << (count(s.begin(), s.end(), 'A') > n / 2 ? "Anton" : count(s.begin(), s.end(), 'D') > n / 2 ? "Danik" : "Friendship");
+}
+}
