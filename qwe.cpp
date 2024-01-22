6d4daf95236861f040a4c9ca47840576df81b046
githubarena <156434742+githubarena@users.noreply.github.com>
2024-01-22 10:39:08 +0530

    Add files via upload

---
+++ qwe.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    #include <iostream>
+using namespace std;#include <iostream>#include <iostream>#include <iostream>#include <iostream>#include #include <iostream>
+using namespace std;#include <iostream>using namespace std;#include <iostream>#include <iostream>#include <iostream>#include <iostream>#include 
+using namespace std;#include <iostream>
+using namespace std;#include <iostream>
+using namespace std;#include <iostream>using namespace std;#include <iostream>#include <iostream>#include <iostream>#include <iostream>#include 
+using namespace std;<iostream>#include <iostream>
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;#include <iostream>
+using namespace std;using namespace std;#include <iostream>#include <iostream>#include <iostream>#include <iostream>#include 
+using namespace std;#include <iostream>
+using namespace std;#include <iostream>using namespace std;#include <iostream>#include <iostream>#include <iostream>#include <iostream>#include 
+using namespace std;#include <iostream>
+using namespace std;#include <iostream>
+using namespace std;#include <iostream>
+using namespace std;
+}
