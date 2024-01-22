cd26073ec58050fb5924989c38505333a9365614
githubarena <156434742+githubarena@users.noreply.github.com>
2024-01-22 10:02:54 +0530

    Create the.cpp

---
+++ 800/the.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << (count(s.begin(), s.end(), 'A') > n / 2 ? "Anton" : count(s.begin(), s.end(), 'D') > n / 2 ? "Danik" : "Friendship") >>> cknddnkndndkdkdkndkndndkndkndkdkdknddnkd;
+}
